3731999ef2530ab28a294461a1e74a1044c3d240
xiaotie <xiaotie@dianhun.cn>
2019-04-01 09:16:08 +0800

    rename

--- util/reflect.go
+++ util/reflect.go
@@ -2,11 +2,11 @@ package util
 
 import "reflect"
 
-type rfl struct {}
+type rfl struct{}
 
 var Rfl = &rfl{}
 
-func (this *rfl)UnptrValue(ins interface{}) reflect.Value {
+func (this *rfl) UnptrValue(ins interface{}) reflect.Value {
 	val := reflect.ValueOf(ins)
 	for val.Kind() == reflect.Ptr {
 		val = val.Elem()
@@ -14,11 +14,10 @@ func (this *rfl)UnptrValue(ins interface{}) reflect.Value {
 	return val
 }
 
-func (this *rfl)UnptrType(ins interface{}) reflect.Type {
+func (this *rfl) UnptrType(ins interface{}) reflect.Type {
 	val := reflect.TypeOf(ins)
 	for val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
 	return val
 }
-
